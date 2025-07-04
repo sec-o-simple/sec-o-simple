88be093083d3130077d6244048d66d25cd4fe862
Rene Schultheis <schultheisrene@gmail.com>
2025-07-04 08:54:34 +0200

    fix prettier issue

--- src/utils/csafExport/csafExport.ts
+++ src/utils/csafExport/csafExport.ts
@@ -50,7 +50,8 @@ export function createCSAFDocument(documentStore: TDocumentStore) {
         contact_details:
           documentStore.documentInformation.publisher.contactDetails,
         issuing_authority:
-          documentStore.documentInformation.publisher.issuingAuthority || undefined,
+          documentStore.documentInformation.publisher.issuingAuthority ||
+          undefined,
         name: documentStore.documentInformation.publisher.name,
         namespace: documentStore.documentInformation.publisher.namespace,
       },
