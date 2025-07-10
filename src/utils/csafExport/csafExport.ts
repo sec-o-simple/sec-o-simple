e53953f1d9c17ed5e4be975a04e1453034a1de1a
Rene Schultheis <schultheisrene@gmail.com>
2025-07-10 15:04:08 +0200

    load the version dynamically to use the same as the navigation sidebar

--- src/utils/csafExport/csafExport.ts
+++ src/utils/csafExport/csafExport.ts
@@ -1,3 +1,4 @@
+import pck from '@/../package.json'
 import { TAcknowledgmentOutput } from '@/routes/document-information/types/tDocumentAcknowledgments'
 import { calculateBaseScore, calculateQualScore } from 'cvss4'
 import { download } from '../download'
@@ -24,7 +25,7 @@ export function createCSAFDocument(documentStore: TDocumentStore) {
         generator: {
           date: currentDate,
           engine: {
-            version: '0.0.1',
+            version: pck.version,
             name: 'Sec-O-Simple',
           },
         },
