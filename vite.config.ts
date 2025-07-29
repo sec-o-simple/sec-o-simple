7f86b9f7d649d48e6d1e9052864da1b21fc3b699
Stefan Nemeth <stefan@bizfactory.tech>
2025-07-29 22:14:23 +0200

    Add vitest and actions for coverage

--- vite.config.ts
+++ vite.config.ts
@@ -23,7 +23,8 @@ export default defineConfig({
     setupFiles: ['./src/test/setup.ts'],
     coverage: {
       provider: 'v8',
-      reporter: ['text', 'json', 'html', 'lcov'],
+      reporter: ['text', 'json', 'json-summary', 'html', 'lcov'],
+      reportOnFailure: true,
       exclude: [
         'node_modules/',
         'src/test/',
