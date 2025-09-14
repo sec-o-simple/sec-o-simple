1e1fd16c74e0907538cd54cacaf4e0b800684547
Stefan Nemeth <stefan@bizfactory.tech>
2025-09-14 22:42:15 +0200

    chore: Add e2e test and action to test it

--- cypress.config.js
+++ cypress.config.js
@@ -1,4 +1,5 @@
 import { defineConfig } from 'cypress'
+import fs from 'fs'
 
 export default defineConfig({
   e2e: {
@@ -6,6 +7,23 @@ export default defineConfig({
     specPattern: 'tests/e2e/**/*.cy.js',
     supportFile: false,
     fixturesFolder: 'tests/fixtures',
+    defaultCommandTimeout: 8000,
+    viewportWidth: 1280,
+    viewportHeight: 800,
     screenshotsFolder: 'tests/screenshots',
+    setupNodeEvents(on, config) {
+      on('task', {
+        fileExists(filename) {
+          return fs.existsSync(filename)
+        },
+        deleteFileIfExists(filename) {
+          if (fs.existsSync(filename)) {
+            fs.unlinkSync(filename)
+          }
+          return true
+        },
+      })
+      return config
+    },
   },
 })
